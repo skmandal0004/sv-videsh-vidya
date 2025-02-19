22648dbbf9591a97e351437f0edd1322f62bd18d
Anju0706 <anjalinirmna@gmail.com>
2025-02-19 19:02:51 +0530

    anjali

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,38 @@
-import { useState } from 'react'
-import NavbarComponents from './Components/NavbarComponents'
-import HeroComponents from './Components/Home/HeroComponents'
-import SignComponents from './Components/Home/SignComponents'
-import WhyComponents from './Components/Home/WhyComponents'
-import ReasonComponents from './Components/Home/ReasonComponents'
-import StudyComponents from './Components/Home/StudyComponents'
-import TrainingComponents from './Components/Home/TrainingComponents'
-import YoutubeComponents from './Components/Home/YoutubeComponents'
-import RatingComponents from './Components/Home/RatingComponents'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavbarComponents from "./Components/NavbarComponents";
+import HeroComponents from "./Components/Home/HeroComponents";
+import SignComponents from "./Components/Home/SignComponents";
+import WhyComponents from "./Components/Home/WhyComponents";
+import ReasonComponents from "./Components/Home/ReasonComponents";
+import StudyComponents from "./Components/Home/StudyComponents";
+import TrainingComponents from "./Components/Home/TrainingComponents";
+import YoutubeComponents from "./Components/Home/YoutubeComponents";
+import RatingComponents from "./Components/Home/RatingComponents";
+import AboutComponents from "./Components/About/AboutComponents";
 
-
+// Home Page
+const Home = () => (
+  <>
+    <HeroComponents />
+    <WhyComponents />
+    <ReasonComponents />
+    <StudyComponents />
+    <TrainingComponents />
+    <YoutubeComponents />
+    <RatingComponents />
+  </>
+);
 
 function App() {
-  
-
   return (
-    <>
-     <NavbarComponents/> 
-     <HeroComponents/>
-     {/* <SignComponents/> */}
-     <WhyComponents/>
-     <ReasonComponents/>
-     <StudyComponents/>
-     <TrainingComponents/>
-     <YoutubeComponents/>
-     <RatingComponents/>
-    </>
-  )
+    <Router>
+      <NavbarComponents />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutComponents />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
