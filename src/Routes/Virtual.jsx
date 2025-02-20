66004160f039bd1ee9e00233fae333534c232fce
Anju0706 <anjalinirmna@gmail.com>
2025-02-20 13:59:53 +0530

    anjali

---
+++ src/Routes/Virtual.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import VirtualComponents from '../Components/Virtual/VirtualComponents'
+import OnlineComponents from '../Components/Virtual/OnlineComponents'
+import LangComponents from '../Components/Virtual/LangComponents'
+
+const Virtual = () => {
+  return (
+    <div>
+    <VirtualComponents/>
+      <OnlineComponents/>
+      <LangComponents/>
+    </div>
+  )
+}
+
+export default Virtual
