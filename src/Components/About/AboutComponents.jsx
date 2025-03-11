ee0165f1c1d4ed684ec4737b52cbec154fdbfa31
skmandal0004 <skmandal0004@gmail.com>
2025-03-11 17:30:51 +0530

    push 2

--- src/Components/About/AboutComponents.jsx
+++ src/Components/About/AboutComponents.jsx
@@ -8,7 +8,7 @@ const AboutComponents = () => {
         <h2 className="text-5xl md:text-4xl font-extrabold mb-6">About <span className="text-white">Us</span></h2>
         {/* Description */}
         <p className="text-lg leading-relaxed text-gray-50">
-          <strong className="text-indigo-950 font-bold">S V Videsh Vidya Enterprises</strong> iss dedicated to helping students from all over India to realize their dream of 
+          <strong className="text-indigo-950 font-bold">S V Videsh Vidya Enterprises</strong> is dedicated to helping students from all over India to realize their dream of 
           getting their qualification from an institute that is best suited for their requirements, skills, and knowledge 
           they strive to acquire.
         </p>
